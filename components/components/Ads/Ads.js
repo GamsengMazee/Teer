7eeacbc09352cdd242b933a8bff9aaf898722f9b
Gamseng M Marak <47160012+GamsengMazee@users.noreply.github.com>
2023-05-30 11:46:04 +0530

    Delete components/components directory

--- components/components/Ads/Ads.js
+++
@@ -1,9 +0,0 @@
-import classes from "./Ads.module.css"
-
-export default function Ads(){
-   return(
-    <main className={classes.ads}>
-
-    </main>
-   )
-}
--- components/components/Layout/Backdrop.js
+++
@@ -1,6 +0,0 @@
-import classes from "./backdrop.module.css";
-function Backdrop(props) {
-  return <div className={classes.backdrop} onClick={props.drawerClose}></div>;
-}
-
-export default Backdrop;
